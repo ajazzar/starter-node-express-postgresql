c0013f9d6afc42e65d1bcc2cf9f63dddb088b360
Aaron <ajazzar@mail.usf.edu>
2022-06-24 19:38:57 -0400

    last commit

--- knexfile.js
+++ knexfile.js
@@ -3,45 +3,20 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
+ const path = require("path");
+ require("dotenv").config();
+ const { DATABASE_URL } = process.env;
+ module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "postgresql",
+    connection: DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+         directory: path.join(__dirname, "src", "db", "migrations"),
+      },
+      seeds: {
+        directory: path.join(__dirname, "src", "db", "seeds"),
+        },
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
